1244f39f7a7a0838cba61e9de2201cc2a42054c6
Aditya Khode <113977001+adityakhode@users.noreply.github.com>
2025-08-16 17:19:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import os
 app = Flask(__name__)
 
 # Fetch values from environment variables
-type_ = os.getenv("FIREBASE_TYPE")
-project_id = os.getenv("FIREBASE_PROJECT_ID")
-private_key_id = os.getenv("FIREBASE_PRIVATE_KEY_ID")
-private_key = os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")  # fix multiline issue
-client_email = os.getenv("FIREBASE_CLIENT_EMAIL")
-client_id = os.getenv("FIREBASE_CLIENT_ID")
-auth_uri = os.getenv("FIREBASE_AUTH_URI")
-token_uri = os.getenv("FIREBASE_TOKEN_URI")
-auth_provider_x509_cert_url = os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
-client_x509_cert_url = os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-universe_domain = os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+type_ = os.getenv("type")
+project_id = os.getenv("project_id")
+private_key_id = os.getenv("private_key_id")
+private_key = os.getenv("private_key")
+client_email = os.getenv("client_email")
+client_id = os.getenv("client_id")
+auth_uri = os.getenv("auth_uri")
+token_uri = os.getenv("token_uri")
+auth_provider_x509_cert_url = os.getenv("auth_provider_x509_cert_url")
+client_x509_cert_url = os.getenv("client_x509_cert_url")
+universe_domain = os.getenv("universe_domain")
 
 # Save them back into dictionary (same format as service account JSON)
 firebase_vars = {
